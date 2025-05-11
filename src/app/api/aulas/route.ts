4a6d867f4bb672bb6eaaaec9cbe9d43c51300593
Manoela Urias <maanoelaurias@gmail.com>
2025-05-10 22:04:47 -0300

    configurando banco de dados

--- src/app/api/aulas/route.ts
+++ src/app/api/aulas/route.ts
@@ -1,16 +1,44 @@
 "use server";
 
 import { getAulas } from "@/actions/getAulas";
+import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { z } from "zod"
 
 
-export async function GET(){
+const aulaSchema = z.object({
+    data: z.string(),
+    tecnica: z.enum(['guarda', 'raspagem', 'finalizacao', 'drill', 'fisico']),
+    dificuldade: z.enum(['facil', 'media', 'dificil']),
+})
+
+export async function POST(req: Request) {
+    try {
+        const body = await req.json();
+        const dados = aulaSchema.parse(body);
+
+        const novaAula = await prisma.aula.create({
+            data: {
+                data: new Date(dados.data),
+                tecnica: dados.tecnica,
+                dificuldade: dados.dificuldade,
+            }
+        })
+
+        return NextResponse.json(novaAula);
+    } catch (error) {
+        console.error("Erro ao criar aula:", error);
+        return NextResponse.json({ error: "Erro ao criar aula" }, { status: 500 })
+    }
+}
+
+export async function GET() {
     try {
         const aulas = await getAulas();
         return NextResponse.json(aulas);
 
     } catch (error) {
         console.error('Erro ao buscar aulas:', error);
-        return NextResponse.json({message: 'Erro ao buscar aulas'}, { status: 500 });
+        return NextResponse.json({ message: 'Erro ao buscar aulas' }, { status: 500 });
     }
 }
---
+++ src/hooks/enviarAula.ts
@@ -0,0 +1,20 @@
+async function enviaAula(data: Date, tecnica: string, dificuldade: string) {
+    const res = await fetch("/api/aulas", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+
+        body: JSON.stringify({
+            data: data.toISOString(),
+            tecnica,
+            dificuldade,
+        }),
+    })
+
+    if (!res.ok) {
+        throw new Error("Erro ao enviar aula");
+    }
+
+    return res.json();
+}
